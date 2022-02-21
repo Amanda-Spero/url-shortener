fb1259daa8c9ce5fce7bad2cf8a14e84c5b16eac
Amanda Spero <amandaspero@Amandas-MacBook-Air.local>
2022-02-20 22:15:37 -0600

    enought styling to be readable

--- src/MyUrls/my-url-card.js
+++ src/MyUrls/my-url-card.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
 const MyUrlCard = ({url, key}) => {
-
-
     return (
-        <div key={key}>
-            <div>
-                <span>Pretty Url</span>
-                <span>{url.prettyUrl}</span>
+        <div className="my-url-card" key={key}>
+            <div className="my-url-card-type">
+                <div className="my-url-card-header">Pretty Url</div>
+                <div>{url.prettyUrl}</div>
             </div>
-            <div>
-                <span>Short Url</span>
-                <span>{url.shortUrl}</span>
+            <div className="my-url-card-type">
+                <div className="my-url-card-header">Short Url</div>
+                <div>{url.shortUrl}</div>
             </div>
-            <div>
-                <span>Original Url</span>
-                <span>{url.longUrl}</span>
+            <div className="my-url-card-type">
+                <div className="my-url-card-header">Original Url</div>
+                <div>{url.longUrl}</div>
             </div>
         </div>
     )
--- src/MyUrls/my-urls.js
+++ src/MyUrls/my-urls.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import MyUrlCard from './/my-url-card';
+import './my-urls-styles.css';
 
 const MyUrls = ({urlList}) => {
     return (
-        <>
+        <div className="my-urls-container">
             <div>My Urls List</div>
             {urlList && urlList.map((url, index) => {
                 return <MyUrlCard url={url} key={index} />;
             })}
-        </>
+        </div>
     );
 }
 
