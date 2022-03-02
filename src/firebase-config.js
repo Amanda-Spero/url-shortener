b7de848c6566e4e5797477031a336ff2c4cef899
Amanda Spero <amandaspero@Amandas-MacBook-Air.local>
2022-03-02 08:29:18 -0600

    test

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,14 +7,7 @@ import {getFirestore} from '@firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyClC31HwgjOLcfM73bUARdK6vYJPwBjj88",
-  authDomain: "url-shortener-a885f.firebaseapp.com",
-  databaseURL: "https://url-shortener-a885f-default-rtdb.firebaseio.com",
-  projectId: "url-shortener-a885f",
-  storageBucket: "url-shortener-a885f.appspot.com",
-  messagingSenderId: "819593426329",
-  appId: "1:819593426329:web:a703882d12c979d69ccf08",
-  measurementId: "G-X76T4423MV"
+
 };
 
 // Initialize 
